ccc48a0b9191ba4b3feb87167ad9921c42c28505
Thomasjoseph2 <tj089502@gmail.com>
2023-09-27 16:03:14 +0530

    completed

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -18,4 +18,13 @@ export class Company {
             lng: faker.location.longitude()
         }
     }
+
+    markerContent(): string {
+        return `
+          <div>
+            <h1>Company Name: ${this.companyName}</h1>
+            <h3>Catchphrase: ${this.catchPhrase}</h3>
+          </div>
+        `;
+      }
 }
--- maps/src/Users.ts
+++ maps/src/Users.ts
@@ -3,15 +3,19 @@ import { faker } from "@faker-js/faker";
 export class User {
     name: string;
     location: {
-        lat: number;
-        lng: number;
+      lat: number;
+      lng: number;
     };
-
+  
     constructor() {
-        this.name = faker.person.firstName();
-        this.location = {
-            lat: faker.location.latitude(),
-            lng: faker.location.longitude()
-        }
+      this.name = faker.person.firstName();
+      this.location = {
+        lat: faker.location.latitude(),
+        lng: faker.location.longitude()
+      };
     }
-} 
+  
+    markerContent(): string {
+      return `User Name: ${this.name}`;
+    }
+  }
