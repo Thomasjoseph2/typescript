b707b0aca78a7278527af52f0fea98aa1a3005c7
Thomasjoseph2 <tj089502@gmail.com>
2023-09-27 14:50:50 +0530

    google map implimented

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,12 @@
-import { User } from "./Users";
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user= new User()
-
-console.log(user);
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 1,
+    center: {
+      lat: 0,
+      lng: 0,
+    },
+  });
+  
